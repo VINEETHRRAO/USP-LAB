e4cd3fdcb6422c5eb276f2854deb342a048a393b
VINEETHRRAO <71490722+VINEETHRRAO@users.noreply.github.com>
2021-12-01 22:32:58 +0530

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -4,7 +4,7 @@ read year
 if [ `expr $year % 400` -eq 0 ] ; then
 echo It is a leap year
 elif [ `expr $year % 100` -eq 0 ] ; then
-echo It is a leap year
+echo Not a leap year
 elif [ `expr $year % 4` -eq 0 ] ; then
 echo It is a leap year
 else 
